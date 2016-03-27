3fec3d59dae64961e187a64c861032188632d819
Derry Birkett <derry@d2tstudio.com>
2016-03-27 11:54:18 +0200

    Update jribbble and main

--- js/main.js
+++ js/main.js
@@ -1,121 +1,135 @@
+      // CONFIG
+      $.jribbble.setToken('a129781e88f49589bae19210ceb3020bd7a6188864d1e20e26923f21b4ecb6a0');
 
-			var callback = function (listDetails) {
-			
+
+			var callback = function (shots) {
+
+
 						/*---------------------------------------------------------------------------------
 						 	Prepare container
-						 
-						 
-						 
+
+
+
 						 ---------------------------------------------------------------------------------*/
 
 						 		shotsArr = [];
 
 						/*---------------------------------------------------------------------------------
 						 	The Loop
-						 
-						 
-						 
+
+
+
 						 ---------------------------------------------------------------------------------*/
-					            $.each(listDetails.shots, function (i, shot) {
-					                
+
+                shots.forEach(function(shot) {
+
 					                // Do calculation for shot power
 							        var rawpower = shot.likes_count / shot.views_count*100;
 							        var flatpower = Math.round(rawpower*100)/100;
-					                
+
 					                // Create (clone) jQuery ibject
 							                li = $('#shotTemplate').clone();
-				
+
 								                $('#shotTemplate').hide();
-								                
+
 								                // Update clone data
 								                li.show();
 								                li.attr({id:shot.id}); 									// set ID to shot ID
-								                
+
 								                // Image
-								                li.find('.scrshot').attr('src',shot.image_url); 		// update thumbnail
-								                li.find('a').attr('href',shot.url)
+								                li.find('.scrshot').attr('src',shot.images.normal); 		// update thumbnail
+								                li.find('a').attr('href',shot.html_url)
 
 												// Shot article
 								                li.find('.title').text(shot.title); 					// update title
-								                li.find('.username').html('<a href="'+shot.player.url+'">'+shot.player.username+'</a>');
+//								                li.find('.username').html('<a href="'+shot.user.html_url+'">'+shot.user.username+'</a>');
 
 												// Stats
 								                li.find('.views_count').text(shot.views_count); 		// update stats
-												li.find('.likes_count').text(shot.likes_count);
+																li.find('.likes_count').text(shot.likes_count);
 
 												// Scores
 								                li.find('.score').text(flatpower+"%"); 					// update score
-								                
+
 									                // Humanise the scores
 									                if(flatpower<1) li.find('.tab').html("<i class='icon-remove icon-white'></i>");
 									                if(flatpower<10) li.find('.tab').html("<i class='icon-heart icon-white'></i>");
 									                if(flatpower>10) li.find('.tab').html("<i class='icon-star icon-white'></i>");
-													if(flatpower>15) li.find('.tab').html("<i class='icon-fire icon-white'></i>");
+																	if(flatpower>15) li.find('.tab').html("<i class='icon-fire icon-white'></i>");
 
 								                //li.find('.hoverlay').hover(function(){$(this).fadeIn(1000);},function(){ $(this).fadeOut(1000);});
-								                
+
 								                // Object Vars
 								                li.rawpower		= rawpower;
 								                li.flatpower 	= flatpower;
-								                
-		
+
+
 									// Put the new jQuery objects into an array
 						            shotsArr.push(li);
-		
+
 					            });
 
 						/*---------------------------------------------------------------------------------
 						 	Sort
-						 
-						 
-						 
+
+
+
 						 ---------------------------------------------------------------------------------*/
-						
+
 								function makeNumericCmp(property) {
 								    return function (a, b) {
 								        return parseInt(b[property]) - parseInt(a[property]);
 								    };
 								}
-								
+
 								shotsArr.sort(makeNumericCmp('rawpower'));
 
 						/*---------------------------------------------------------------------------------
 						 	Append
-						 
-						 
-						 
+
+
+
 						 ---------------------------------------------------------------------------------*/
 						shotsArr.join();
 						$('#shotsByList').append(shotsArr).fadeIn(1000);
 
 			        }
-			
-			
+
+
 					/*---------------------------------------------------------------------------------
 					 	Options
-					 
-					 
-					 
+
+
+
 					 ---------------------------------------------------------------------------------*/
 
 
 					 /* STANDARDS
-					---------------------------*/					
+					---------------------------*/
 					//$(".hoverlay").hover(function(){$(this).fadeOut(100);$(this).fadeIn(500);});
 
 					/* FUNCTIONS
 					---------------------------*/
-					
+
 						// Fetch List
 						function fetchList(filter) {
 							//$('#shotsByList').html('');
-							$.jribbble.getShotsByList('popular', callback, {page: 1, per_page: 15});
+							$.jribbble.shots('debuts').then(function(shots){
+                 // loadShots(shots);
+                  callback(shots);
+              });
 						}
-						
+
 						function fetchPlayerShots(filter) {
 							$('#shotsByList').empty();
-							$.jribbble.getShotsByPlayerId(filter, callback, {page: 1, per_page: 15})
-						}
+
+console.log("player"+PlayerName);
+							$.jribbble.users(filter).shots().then(function(shots){
+                 // loadShots(shots);
+                  callback(shots);
+              });
+  
+					}
 
 						// Read a page's GET URL variables and return them as an associative array.
 						function getUrlVars()
@@ -130,26 +144,25 @@
 							    }
 							    return vars;
 							}
-							
-					
+
+
 					/* Triggers
 					---------------------------*/
 						// player
 						$('#PlayerName').keyup(function() {
 							var player = $('#PlayerName').val();
-							$('#msg').text('Fetching shots from '+player).fadeIn(1000);
+							$('#msg').text('Fetching shots from ' + player).fadeIn(1000);
 							fetchPlayerShots(player)
 						});
-					
-					
+
+
 					/* Launch default list
 					---------------------------*/
 					var PlayerName = getUrlVars()['PlayerName'];
-
+console.log("plauer"+PlayerName);
 					if(PlayerName)
 						{
 							fetchPlayerShots(PlayerName);
 						} else {
-							fetchList('popular');
+							fetchList('debuts');
 						}
-					
